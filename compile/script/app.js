90de3fdcd2bc5f3dbeffa3ef6fb59fdd004727c9
jason <jason@wizmacau.com>
2019-11-22 18:34:19 +0800

    gulp

--- compile/script/app.js
+++ compile/script/app.js
@@ -1,10 +1,29 @@
-'use strict';
-
-function init() {
-    console.log('Init');
-}
-
-document.addEventListener('load', (e) => {
+window.onload = function (e) {
     console.log(e);
     init();
-});
+}
+
+function init() {
+    let arr = 1;
+    console.log(arr);
+}
+
+// 图片加载完成
+function imgLoad(ele) {
+    let img = [],
+        flag = 0,
+        mulitImg = ele;
+    let imgTotal = mulitImg.length;
+    for (let i = 0; i < imgTotal; i++) {
+        img[i] = new Image();
+        img[i].src = mulitImg[i].src;
+        let pic = img[i];
+        $(pic).on('load', () => {
+            $(this).remove();
+            flag++;
+            if(flag == imgTotal) {
+                // do something
+            }
+        })
+    }
+}
