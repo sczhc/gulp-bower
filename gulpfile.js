651edb3f47775498e488a3966bf28e33ebb3bf2a
jason <jason@wizmacau.com>
2019-11-25 15:02:03 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
-var { task,src,dest,series,parallel,watch } = require('gulp'),
+var { task, src, dest, series, parallel, watch } = require('gulp'),
     es2015 = require('babel-preset-es2015'),
     plugins = require('gulp-load-plugins')();
 
 var publishDir = 'web/dist';
 
-task('less', function() {
+task('less', function () {
     return src('compile/less/common.less')
         .pipe(plugins.less()) // 把less转成css
-        .pipe(plugins.concatCss('main.min.css')) // 把所有css文件合成一个 main.min.css文件
-        .pipe(plugins.cssnano()) // 对css文件压缩
+        .pipe(plugins.concat('main.min.css')) // 把所有css文件合成一个 main.min.css文件
         .pipe(plugins.autoprefixer()) // 自动添加浏览器前缀
+        .pipe(plugins.cssnano()) // 对css文件压缩
         .pipe(dest(publishDir + '/css'))
 })
 
-task('scripts', function() {
+task('scripts', function () {
     return src('compile/script/**/*.js')
         .pipe(plugins.babel({ presets: [es2015] })) // 把es6 => es5
         .pipe(plugins.concat('main.min.js')) // 把所有js文件合成一个 main.min.js文件
@@ -23,12 +23,12 @@ task('scripts', function() {
         .pipe(dest(publishDir + '/js'))
 })
 
-task('clean-bower-files', function() {
+task('clean-bower-files', function () {
     return src(publishDir + '/bower/*', { read: false })
         .pipe(plugins.clean({ force: true }))
 })
 
-task('bower-files', function() {
+task('bower-files', function () {
     return src('bower.json')
         .pipe(plugins.mainBowerFiles({
             overrides: {
